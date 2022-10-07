6376df84955605df930f0edaa2e1713734ff789b
Nathan Solomon <nathansolomon@Nathans-MacBook-Air.local>
2022-10-07 13:26:50 -0400

    First commit. Hash values from server are sending to client

---
+++ fileclient.cpp
@@ -0,0 +1,306 @@
+// --------------------------------------------------------------
+//
+//                        pingclient.cpp
+//
+//        Author: Noah Mendelsohn         
+//   
+//
+//        This is a simple client, designed to illustrate use of:
+//
+//            * The C150DgmSocket class, which provides 
+//              a convenient wrapper for sending and receiving
+//              UDP packets in a client/server model
+//
+//            * The c150debug interface, which provides a framework for
+//              generating a timestamped log of debugging messages.
+//              Note that the socket classes described above will
+//              write to these same logs, providing information
+//              about things like when UDP packets are sent and received.
+//              See comments section below for more information on 
+//              these logging classes and what they can do.
+//
+//
+//        COMMAND LINE
+//
+//              pingclient <servername> <msgtxt>
+//
+//
+//        OPERATION
+//
+//              pingclient will send a single UDP packet
+//              to the named server, and will wait (forever)
+//              for a single UDP packet response. The contents
+//              of the packet sent will be the msgtxt, including
+//              a terminating null. The response message
+//              is checked to ensure that it's null terminated.
+//              For safety, this application will use a routine 
+//              to clean up any garbage characters the server
+//              sent us, (so a malicious server can't crash us), and
+//              then print the result.
+//
+//              Note that the C150DgmSocket class will select a UDP
+//              port automatically based on the user's login, so this
+//              will (typically) work only on the test machines at Tufts
+//              and for COMP 150-IDS who are registered. See documention
+//              for the comp150ids getUserPort routine if you are 
+//              curious, but you shouldn't have to worry about it.
+//              The framework automatically runs on a separate port
+//              for each user, as long as you are registerd in the
+//              the student port mapping table (ask Noah or the TAs if
+//              the program dies because you don't have a port).
+//
+//        LIMITATIONS
+//
+//              This version does not timeout or retry when packets are lost.
+//
+//
+//       Copyright: 2012 Noah Mendelsohn
+//     
+// --------------------------------------------------------------
+
+
+#include "c150dgmsocket.h"
+#include "c150debug.h"
+#include <fstream>
+
+using namespace std;          // for C++ std library
+using namespace C150NETWORK;  // for all the comp150 utilities 
+
+// forward declarations
+void checkAndPrintMessage(ssize_t readlen, char *buf, ssize_t bufferlen);
+void setUpDebugLogging(const char *logname, int argc, char *argv[]);
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                    Command line arguments
+//
+// The following are used as subscripts to argv, the command line arguments
+// If we want to change the command line syntax, doing this
+// symbolically makes it a bit easier.
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                           main program
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+int 
+main(int argc, char *argv[]) {
+
+    //
+    // Variable declarations
+    //
+    ssize_t readlen;              // amount of data read from socket
+    char incomingMessage[100];   // received message data
+
+    //
+    //  Set up debug message logging
+    //
+    setUpDebugLogging("pingclientdebug.txt",argc, argv);
+
+    //
+    // Make sure command line looks right
+    //
+    if (argc != 5) {
+        fprintf(stderr,"Correct syntxt is: %s <server> <networknastiness> <filenastiness> <srcdir>\n", argv[0]);
+        exit(1);
+    }
+
+    string serverName = argv[1];
+    // int networkNastiness = atoi(argv[2]);
+    // int filenastiness = atoi(argv[3]);
+    string srcdir = argv[4];
+
+    //
+    //
+    //        Send / receive / print 
+    //
+    try {
+
+        // Create the socket
+        c150debug->printf(C150APPLICATION,"Creating C150DgmSocket");
+        C150DgmSocket *sock = new C150DgmSocket();
+
+        // Tell the DGMSocket which server to talk to
+        sock -> setServerName((char*)serverName.c_str());  
+        sock->turnOnTimeouts(3000);
+
+        /// assume file copying is done here 
+
+        // // Send the message to the server
+        string randomMessage = "random Message";
+        const char * randomMessageArr = randomMessage.c_str();
+        c150debug->printf(C150APPLICATION,"%s: Writing message: \"%s\"", argv[0], randomMessageArr);
+        sock -> write(randomMessageArr, strlen(randomMessageArr)+1); // +1 includes the null
+
+        // Read the response from the server
+        c150debug->printf(C150APPLICATION,"%s: reading server response:",
+                          argv[0]);
+
+        readlen = sock -> read(incomingMessage, sizeof(incomingMessage));
+        while(!sock -> timedout()) {
+            checkAndPrintMessage(readlen, incomingMessage, sizeof(incomingMessage));
+            readlen = sock -> read(incomingMessage, sizeof(incomingMessage));
+        }
+        // Check and print the incoming message
+     
+
+    }
+
+    //
+    //  Handle networking errors -- for now, just print message and give up!
+    //
+    catch (C150NetworkException& e) {
+        // Write to debug log
+        c150debug->printf(C150ALWAYSLOG,"Caught C150NetworkException: %s\n",
+                          e.formattedExplanation().c_str());
+        // In case we're logging to a file, write to the console too
+        cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation()\
+                        << endl;
+    }
+
+    return 0;
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                     checkAndPrintMessage
+//
+//        Make sure length is OK, clean up response buffer
+//        and print it to standard output.
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+
+
+void
+checkAndPrintMessage(ssize_t readlen, char *msg, ssize_t bufferlen) {
+    // 
+    // Except in case of timeouts, we're not expecting
+    // a zero length read
+    //
+    if (readlen == 0) {
+        throw C150NetworkException("Unexpected zero length read in client");
+    }
+
+    // DEFENSIVE PROGRAMMING: we aren't even trying to read this much
+    // We're just being extra careful to check this
+    if (readlen > (int)(bufferlen)) {
+        throw C150NetworkException("Unexpected over length read in client");
+    }
+
+    //
+    // Make sure server followed the rules and
+    // sent a null-terminated string (well, we could
+    // check that it's all legal characters, but 
+    // at least we look for the null)
+    //
+    if(msg[readlen-1] != '\0') {
+        throw C150NetworkException("Client received message that was not null terminated");     
+    };
+
+    //
+    // Use a routine provided in c150utility.cpp to change any control
+    // or non-printing characters to "." (this is just defensive programming:
+    // if the server maliciously or inadvertently sent us junk characters, then we 
+    // won't send them to our terminal -- some 
+    // control characters can do nasty things!)
+    //
+    // Note: cleanString wants a C++ string, not a char*, so we make a temporary one
+    // here. Not super-fast, but this is just a demo program.
+    string s(msg);
+    cleanString(s);
+
+    // Echo the response on the console
+
+    c150debug->printf(C150APPLICATION,"PRINTING RESPONSE: Response received is \"%s\"\n", s.c_str());
+    printf("Response received is \"%s\"\n", s.c_str());
+
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                     setUpDebugLogging
+//
+//        For COMP 150-IDS, a set of standards utilities
+//        are provided for logging timestamped debug messages.
+//        You can use them to write your own messages, but 
+//        more importantly, the communication libraries provided
+//        to you will write into the same logs.
+//
+//        As shown below, you can use the enableLogging
+//        method to choose which classes of messages will show up:
+//        You may want to turn on a lot for some debugging, then
+//        turn off some when it gets too noisy and your core code is
+//        working. You can also make up and use your own flags
+//        to create different classes of debug output within your
+//        application code
+//
+//        NEEDSWORK: should be factored into shared code w/pingserver
+//        NEEDSWORK: document arguments
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+void setUpDebugLogging(const char *logname, int argc, char *argv[]) {
+
+    //   
+    //           Choose where debug output should go
+    //
+    // The default is that debug output goes to cerr.
+    //
+    // Uncomment the following three lines to direct
+    // debug output to a file. Comment them
+    // to default to the console.
+    //
+    // Note: the new DebugStream and ofstream MUST live after we return
+    // from setUpDebugLogging, so we have to allocate
+    // them dynamically.
+    //
+    //
+    // Explanation: 
+    // 
+    //     The first line is ordinary C++ to open a file
+    //     as an output stream.
+    //
+    //     The second line wraps that will all the services
+    //     of a comp 150-IDS debug stream, and names that filestreamp.
+    //
+    //     The third line replaces the global variable c150debug
+    //     and sets it to point to the new debugstream. Since c150debug
+    //     is what all the c150 debug routines use to find the debug stream,
+    //     you've now effectively overridden the default.
+    //
+    ofstream *outstreamp = new ofstream(logname);
+    DebugStream *filestreamp = new DebugStream(outstreamp);
+    DebugStream::setDefaultLogger(filestreamp);
+
+    //
+    //  Put the program name and a timestamp on each line of the debug log.
+    //
+    c150debug->setPrefix(argv[0]);
+    c150debug->enableTimestamp(); 
+
+    //
+    // Ask to receive all classes of debug message
+    //
+    // See c150debug.h for other classes you can enable. To get more than
+    // one class, you can or (|) the flags together and pass the combined
+    // mask to c150debug -> enableLogging 
+    //
+    // By the way, the default is to disable all output except for
+    // messages written with the C150ALWAYSLOG flag. Those are typically
+    // used only for things like fatal errors. So, the default is
+    // for the system to run quietly without producing debug output.
+    //
+    c150debug->enableLogging(C150APPLICATION | C150NETWORKTRAFFIC | 
+                             C150NETWORKDELIVERY); 
+}
---
+++ fileserver.cpp
@@ -0,0 +1,270 @@
+
+#include "c150nastydgmsocket.h"
+#include "c150debug.h"
+#include <fstream>
+#include <cstdlib> 
+#include <dirent.h>
+#include <openssl/sha.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+using namespace C150NETWORK;  // for all the comp150 utilities 
+
+
+void setUpDebugLogging(const char *logname, int argc, char *argv[]);
+void checkDirectory(char *dirname);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                           main program
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+int
+main(int argc, char *argv[])
+{
+
+    //
+    // Variable declarations
+    //
+      // amount of data read from socket
+    int networkNastiness;               // how aggressively do we drop packets, etc?
+    int fileNastiness;
+    string targetDirectory;
+    DIR *TARGET;                // Unix descriptor for target
+    struct dirent *targetFile;  // Directory entry for source file
+    char hashVal[20];
+	string builtHashVal = "";
+    ifstream *t;
+    stringstream *buffer;
+    unsigned char obuf[20];
+
+   
+    //
+    // Check command line and parse arguments
+    //
+    if (argc != 4)  {
+        fprintf(stderr,"Correct syntax is: %s <network nastiness> <file nastiness> <targetDIR>\n", argv[0]);
+        exit(1);
+    }
+
+    if (strspn(argv[1], "0123456789") != strlen(argv[1])) {
+        fprintf(stderr,"Network Nastiness %s is not numeric\n", argv[1]);     
+        fprintf(stderr,"Correct syntxt is: %s <nastiness_number>\n", argv[0]);     
+        exit(4);
+    }
+
+    if (strspn(argv[2], "0123456789") != strlen(argv[2])) {
+        fprintf(stderr,"File Nastiness %s is not numeric\n", argv[1]);     
+        fprintf(stderr,"Correct syntxt is: %s <file_nastiness>\n", argv[0]);     
+        exit(4);
+    }
+
+    networkNastiness = atoi(argv[1]);   
+    fileNastiness = atoi(argv[2]);   
+    (void)fileNastiness;
+    targetDirectory = argv[3];
+    setUpDebugLogging("pingserverdebug.txt",argc, argv);
+    c150debug->setIndent("    ");              // if we merge client and server
+    ssize_t readlen;             // amount of data read from socket
+    char incomingMessage[512];
+    try{
+        
+        c150debug->printf(C150APPLICATION,"Creating C150NastyDgmSocket(nastiness=%d)",  networkNastiness);
+        C150DgmSocket *sock = new C150NastyDgmSocket(networkNastiness);
+        bool isFileRecieved = false;
+        while(!isFileRecieved) {
+            readlen = sock -> read(incomingMessage, sizeof(incomingMessage)-1);
+            if (readlen == 0) {
+                c150debug->printf(C150APPLICATION,"Read zero length message, trying again");
+                continue;
+            }
+            // cout << "test 2" << endl;
+           incomingMessage[readlen] = '\0';  // make sure null terminated
+           string incoming(incomingMessage); // Convert to C++ string ...it's slightly
+//                                               // easier to work with, and cleanString
+           isFileRecieved = true;
+           cleanString(incoming);            // c150ids-supplied utility: changes
+//                                               // non-printing characters to .
+           cout << "a file has just been recieved" << endl;
+
+        }
+
+        checkDirectory((char*)targetDirectory.c_str());
+        TARGET = opendir(argv[3]);
+
+        if (TARGET == NULL) {
+            fprintf(stderr,"Error opening target directory %s \n", argv[3]);     
+            exit(8);
+        }
+
+        //
+        //  Loop copying the files
+        //
+        //    copyfile takes name of target file
+        //
+        string path;
+        while ((targetFile = readdir(TARGET)) != NULL) {
+            builtHashVal = "";
+            // skip the . and .. names
+            if ((strcmp(targetFile->d_name, ".") == 0) ||
+            (strcmp(targetFile->d_name, "..")  == 0 )) 
+            continue;          // never copy . or ..
+                //argv[j] needs to be a path
+                path = targetDirectory + "/" + targetFile->d_name;
+                t = new ifstream(path);
+                buffer = new stringstream;
+                *buffer << t->rdbuf();
+                SHA1((const unsigned char *)buffer->str().c_str(), (buffer->str()).length(), obuf);
+                for (int i = 0; i < 20; i++)
+                {
+                    sprintf(hashVal,"%02x",(unsigned int) obuf[i]);
+                    string stringHashVal(hashVal);
+                    builtHashVal += stringHashVal;
+                }
+                 const char* builtHashValArr = builtHashVal.c_str();
+
+                c150debug->printf(C150APPLICATION,"Responding with message=\"%s\"", builtHashValArr);
+
+                sock -> write(builtHashValArr, builtHashVal.length() + 1);
+                //send builtHashVal
+                delete t;
+                delete buffer;
+        }
+        closedir(TARGET);
+    } 
+    catch (C150NetworkException& e) {
+        // Write to debug log
+        c150debug->printf(C150ALWAYSLOG,"Caught C150NetworkException: %s\n",
+                          e.formattedExplanation().c_str());
+        // In case we're logging to a file, write to the console too
+        cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation()\
+                        << endl;
+    }
+    return 4;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+void
+checkDirectory(char *dirname) {
+  struct stat statbuf;  
+  if (lstat(dirname, &statbuf) != 0) {
+    fprintf(stderr,"Error stating supplied source directory %s\n", dirname);
+    exit(8);
+  }
+
+  if (!S_ISDIR(statbuf.st_mode)) {
+    fprintf(stderr,"File %s exists but is not a directory\n", dirname);
+    exit(8);
+  }
+}
+//////////////////////////////////////////////////////////////////
+     
+    //  Set up debug message logging
+    //
+
+
+    //
+    // We set a debug output indent in the server only, not the client.
+    // That way, if we run both programs and merge the logs this way:
+    //
+    //    cat pingserverdebug.txt pingserverclient.txt | sort
+    //
+    // it will be easy to tell the server and client entries apart.
+    //
+    // Note that the above trick works because at the start of each
+    // log entry is a timestamp that sort will indeed arrange in 
+    // timestamp order, thus merging the logs by time across 
+    // server and client.
+    //
+ 
+    // logs, server stuff will be indented
+
+    //
+    // Create socket, loop receiving and responding
+    //
+    
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                     setUpDebugLogging
+//
+//        For COMP 150-IDS, a set of standards utilities
+//        are provided for logging timestamped debug messages.
+//        You can use them to write your own messages, but 
+//        more importantly, the communication libraries provided
+//        to you will write into the same logs.
+//
+//        As shown below, you can use the enableLogging
+//        method to choose which classes of messages will show up:
+//        You may want to turn on a lot for some debugging, then
+//        turn off some when it gets too noisy and your core code is
+//        working. You can also make up and use your own flags
+//        to create different classes of debug output within your
+//        application code
+//
+//        NEEDSWORK: should be factored and shared w/pingclient
+//        NEEDSWORK: document arguments
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+void setUpDebugLogging(const char *logname, int argc, char *argv[]) {
+
+    //   
+    //           Choose where debug output should go
+    //
+    // The default is that debug output goes to cerr.
+    //
+    // Uncomment the following three lines to direct
+    // debug output to a file. Comment them to 
+    // default to the console
+    //  
+    // Note: the new DebugStream and ofstream MUST live after we return
+    // from setUpDebugLogging, so we have to allocate
+    // them dynamically.
+    //
+    //
+    // Explanation: 
+    // 
+    //     The first line is ordinary C++ to open a file
+    //     as an output stream.
+    //
+    //     The second line wraps that will all the services
+    //     of a comp 150-IDS debug stream, and names that filestreamp.
+    //
+    //     The third line replaces the global variable c150debug
+    //     and sets it to point to the new debugstream. Since c150debug
+    //     is what all the c150 debug routines use to find the debug stream,
+    //     you've now effectively overridden the default.
+    //
+    ofstream *outstreamp = new ofstream(logname);
+    DebugStream *filestreamp = new DebugStream(outstreamp);
+    DebugStream::setDefaultLogger(filestreamp);
+
+
+    //
+    //  Put the program name and a timestamp on each line of the debug log.
+    //
+    c150debug->setPrefix(argv[0]);
+    c150debug->enableTimestamp(); 
+
+    //
+    // Ask to receive all classes of debug message
+    //
+    // See c150debug.h for other classes you can enable. To get more than
+    // one class, you can or (|) the flags together and pass the combined
+    // mask to c150debug -> enableLogging 
+    //
+    // By the way, the default is to disable all output except for
+    // messages written with the C150ALWAYSLOG flag. Those are typically
+    // used only for things like fatal errors. So, the default is
+    // for the system to run quietly without producing debug output.
+    //
+    c150debug->enableLogging(C150APPLICATION | C150NETWORKTRAFFIC | 
+                             C150NETWORKDELIVERY); 
+}
+
